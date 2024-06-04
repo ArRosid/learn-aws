5f63f3ec5bac41442ff3eb87dbd4c1c59dbb89ca
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2024-06-04 07:34:04 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,54 @@
+const AWS = require('aws-sdk');
+AWS.config.update( {
+  region: 'us-east-1'
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'Produk';
+const productsPath = '/products';
+
+exports.handler = async function(event) {
+  console.log('Request event: ', event);
+  let response;
+  switch(true) {
+    case event.httpMethod === 'GET' && event.path === productsPath:
+      response = await getProducts();
+      break;
+    default:
+      response = buildResponse(404, '404 Not Found');
+  }
+  return response;
+}
+
+
+async function getProducts() {
+  const params = {
+    TableName: dynamodbTableName
+  }
+  const allProducts = await scanDynamoRecords(params, []);
+
+  return buildResponse(200, allProducts);
+}
+
+async function scanDynamoRecords(scanParams, itemArray) {
+  try {
+    const dynamoData = await dynamodb.scan(scanParams).promise();
+    itemArray = itemArray.concat(dynamoData.Items);
+    if (dynamoData.LastEvaluatedKey) {
+      scanParams.ExclusiveStartkey = dynamoData.LastEvaluatedKey;
+      return await scanDynamoRecords(scanParams, itemArray);
+    }
+    return itemArray;
+  } catch(error) {
+    console.error('Do your custom error handling here. I am just gonna log it: ', error);
+  }
+}
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+}
